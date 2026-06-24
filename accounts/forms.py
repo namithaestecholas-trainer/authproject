e550bbb0fb49a97107cdca2eb1b67a2aa81a798c
Namitha <namithaestecholas@gmail.com>
2026-06-24 22:26:07 +0530

    password change

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
-    confirm_password = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
@@ -12,8 +13,10 @@ class RegisterForm(forms.ModelForm):
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
         confirm_password = cleaned_data.get("confirm_password")
+
         if password != confirm_password:
             raise forms.ValidationError("Passwords do not match")
+
         return cleaned_data
 
 
